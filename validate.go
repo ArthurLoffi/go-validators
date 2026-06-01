6d81f0b4056bfbd84021830b6cd5371f4452e32a
ArthurLoffi <arthurloffisoares@outlook.com>
2026-06-01 19:14:47 -0300

    chore: changed the name of the functions

--- validate.go
+++ validate.go
@@ -4,16 +4,16 @@ import "regexp"
 
 var nameRegex = regexp.MustCompile(`^[a-zA-ZÀ-ÿ\s]+$`)
 
-// ValidateName reports whether the given string contains only valid characters
+// IsName reports whether the given string contains only valid characters
 // (letters and spaces, including accented characters like À-ÿ).
-func ValidateName(s string) bool {
+func IsName(s string) bool {
 	return nameRegex.MatchString(s)
 }
 
-var idRegex = regexp.MustCompile(`^\d+$`)
+var uintRegex = regexp.MustCompile(`^\d+$`)
 
-// ValidateID reports whether the given string is a valid numeric ID.
+// IsUint reports whether the given string is a valid numeric Uint.
 // Rejects any non-numeric input, including SQL injection attempts.
-func ValidateID(s string) bool {
-    return idRegex.MatchString(s)
+func IsUint(s string) bool {
+    return uintRegex.MatchString(s)
 }
